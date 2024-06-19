3ceab56c7845f6e9737426524a55a3f0e47316cb
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-19 12:50:30 -0300

    salvo

---
+++ src/main/java/com/cappeletti/toquedivino/ToquedivinoApplication.java
@@ -0,0 +1,13 @@
+package com.cappeletti.toquedivino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToquedivinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToquedivinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cappeletti/toquedivino/ToquedivinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cappeletti.toquedivino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToquedivinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
